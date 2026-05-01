ac4b9ad3ac370a4cab4e431b699db00e0cf6cb44
XFuture <3480487849@qq.com>
2026-05-01 13:43:23 +0800

    perf(Setting): 将目标帧率从50提升至60并移除垂直同步

--- Assets/C#Scripts/Setting/GameSetting.cs
+++ Assets/C#Scripts/Setting/GameSetting.cs
@@ -7,8 +7,7 @@ public class GameSetting : MonoBehaviour
 {
     private void Awake()
     {
-        Application.targetFrameRate = 50;//固定帧率为50
-        QualitySettings.vSyncCount = 0;//帧率会被锁定为显示器刷新率的整数倍,0为关闭垂直同步
+        Application.targetFrameRate = 60;//固定帧率为60
         Cursor.lockState = CursorLockMode.Locked;//锁定鼠标
         Cursor.visible = false;//隐藏鼠标
     }
